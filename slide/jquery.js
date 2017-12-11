52d64b615d29a27d65d95fc1802cb300847fb984
yuyeqianxun <yunzuomeng@sina.com>
2017-12-11 14:33:43 +0800

    第七篇：简单slide插件

--- slide/jquery.js
+++ slide/jquery.js
@@ -18,8 +18,10 @@
     }
     Slide.prototype = {
         init: function() {
-        	this.creat()
         	let index=1
+        	this.creat()
+        	this.settings.pagination?this.pagination():''
+        	this.add(1)       	
         	const content = $(this.element).find('ul')
         	content.css('width',this.width*this.length)
         	this.autoplay(content,index)
@@ -28,20 +30,37 @@
         	const _this=this
            	timer=setInterval(function(){
 				_this.run(content,index)
-				if(index===5){
+				if(index===_this.length-1){
 					$(content).animate({right:0+'px'}, 0)
 					index=1
 				}else{
 					index++
-				}		
-			},2000);				
+				}
+				_this.add(index)
+			},_this.settings.interval)				
         },
         run: function(data,index) {
         	$(data).animate({right:index*this.width+'px'}, 1000)
         },
         creat: function(){
         	$($(this.element).find('ul li')[0]).clone().appendTo($(this.element).find('ul'))
-        }
+        },
+        add: function(index){
+        	let active=$(this.element).find('ul li')[index-1]
+        	$(active).addClass('active').siblings().removeClass('active')
+        	if(this.settings.pagination){
+        		let conActive=$(this.element).find('ol li')[index-1]
+        		$(conActive).addClass('conActive').siblings().removeClass('conActive')
+        	}
+        },
+        pagination: function(index){
+        	let ol="<ol></ol>"
+        	$(this.element).append(ol)
+        	let li="<li></li>"
+			for(i=1;i<this.length;i++){
+				$(this.element).find('ol').append(li)
+			}			 
+		}       	
     };
     $.fn[pluginName] = function(options) {
         this.each(function() {
@@ -51,5 +70,4 @@
         });
         return this;
     };
-
 })(jQuery, window, document);
